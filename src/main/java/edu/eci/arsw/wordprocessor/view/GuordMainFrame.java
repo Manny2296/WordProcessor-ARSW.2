42f54a8de70752d8d765f7c40bfe16347a4ecc0d
Héctor Fabio Cadavid Rengifo <hector.cadavid@gmail.com>
2016-08-17 16:10:45 -0500

    Update GuordMainFrame.java

--- src/main/java/edu/eci/arsw/wordprocessor/view/GuordMainFrame.java
+++ src/main/java/edu/eci/arsw/wordprocessor/view/GuordMainFrame.java
@@ -7,7 +7,7 @@ package edu.eci.arsw.wordprocessor.view;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFocundException;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FilenameFilter;
 import java.io.IOException;
